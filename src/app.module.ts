27b6e77c780c47f75b731235f0c2250c37e40e3c
Abdelilah Zaidi <abdelilahzaididev@gmail.com>
2023-02-14 14:00:38 +0100

    add an auth module and connexion to database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [AuthModule,TypeOrmModule.forRoot({
+    type: 'mysql',
+    host: 'localhost',
+    port: 3306,
+    username: 'zyadain',
+    password: '14101983',
+    database: 'GCV',
+    entities: [],
+    synchronize: true,
+  }),],
   controllers: [AppController],
   providers: [AppService],
 })
